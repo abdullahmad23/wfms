45883ce64a9d06b1636c75c97a4948e3bc42c1b9
Huzaifa <huzaifatawab33@gmail.com>
2023-11-24 14:45:55 +0500

    varification screen

---
+++ lib/Auth/VarificationScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VarificationScreen extends StatelessWidget {
+  const VarificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('You are Not Verified'),
+      ),
+    );
+  }
+}
--- lib/Screens/Admin/AdminMain.dart
+++ lib/Screens/Admin/AdminMain.dart
@@ -1,10 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:waste/Auth/VarificationScreen.dart';
 
-class AdminMain extends StatelessWidget {
+class AdminMain extends StatefulWidget {
   const AdminMain({super.key});
 
   @override
+  State<AdminMain> createState() => _AdminMainState();
+}
+
+class _AdminMainState extends State<AdminMain> {
+  @override
+  initState() {
+    String id = FirebaseAuth.instance.currentUser!.uid;
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(id)
+        .get()
+        .then((value) => {
+              if (value.data()?['varified'] == false)
+                {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const VarificationScreen(),
+                    ),
+                  )
+                }
+            });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    NavigateToverification() {}
+
     return const Placeholder();
   }
 }
