019bcf8f25b8fe4d540740004f38513156e932c9
abdullah <abdullahmad246@gmail.com>
2023-12-19 11:14:24 +0500

    hotel list calling

--- lib/Screens/Client/ClientHotelList.dart
+++ lib/Screens/Client/ClientHotelList.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:waste/Components/AppLogo.dart';
 
@@ -9,60 +11,94 @@ class ClientHotelList extends StatefulWidget {
 }
 
 class _ClientHotelListState extends State<ClientHotelList> {
+  List<Map> HotelData = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getHotel();
+  }
+
+  getHotel() {
+    FirebaseFirestore.instance
+        .collection("users")
+        .where('type', isEqualTo: "Hotel")
+        .get()
+        .then((hotelList) {
+      print(hotelList);
+      for (var hotel in hotelList.docs) {
+        setState(() {
+          HotelData.add(hotel.data());
+        });
+        print("updated");
+        setState(() {
+          isLoading = false;
+        });
+        print("updated");
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(margin:EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-          AppLogo(),
-          SizedBox(height: 5,),
-          hallcontainer(),
-          SizedBox(height: 5,),
-          hallcontainer(),
-          SizedBox(height: 5,),
-          hallcontainer(),
-          SizedBox(height: 5,),
-          hallcontainer()
-        ],),
-      )
-    );
+    return isLoading
+        ? Center(child: CircularProgressIndicator())
+        : SafeArea(
+            child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                children: [
+                  AppLogo(),
+                  ...HotelData.map((hotel) => hallcontainer(
+                      "${hotel['name']}", "${hotel['img']}", context)),
+                ],
+              ),
+            ),
+          ));
   }
 }
-Widget hallcontainer(){
-  return Container(height: 100,
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      margin: EdgeInsets.only(top: 15),
-      decoration: BoxDecoration(
-          border: Border.all(), borderRadius: BorderRadius.circular(18)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'AL Hamra Shadi Hall',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black),
-              ),
-              SizedBox(height: 8.0,),
-              Text(
-                'Karakoeam Highway \nMarghazar Mansehra',
-                style: TextStyle(
-                  fontSize: 18,
+
+Widget hallcontainer(String hotelName, String Hotelimg, context) {
+  return Container(
+    height: MediaQuery.of(context).size.height * 0.20,
+    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+    margin: EdgeInsets.only(top: 15),
+    decoration: BoxDecoration(
+        border: Border.all(), borderRadius: BorderRadius.circular(18)),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              hotelName,
+              style: TextStyle(
+                  fontSize: 20,
                   fontWeight: FontWeight.w500,
-                  color: Color(0xff4a4951),
-                ),
+                  color: Colors.black),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              'Karakoeam Highway \nMarghazar Mansehra',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Color(0xff4a4951),
               ),
-              SizedBox(height: 8.0,),
-             
-            ],
-          ),
-          SizedBox(child: Image.asset("assets/Mask group.png"))
-        ],
-      ),
-      );
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+          ],
+        ),
+        SizedBox(child: Image.network(Hotelimg)),
+      ],
+    ),
+  );
 }
